59adc7ffd45b2b16d180bccf910f80a69d66ad50
Devmadia <agathaphan@icloud.com>
2020-08-17 22:32:53 -0500

    finalized login for users, installed express-session for sequelize

---
+++ public/javascript/login.js
@@ -0,0 +1,59 @@
+/* create a listener and a function to handle the login submission */ 
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    /* POST the username, email, and password from the form to the server */
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    /* make a fetch() POST request to the /api/users/ */
+    if (username && email && password) {
+        /* conditional to make sure that all fields have values before making the POST request 
+        [insert await before the Promise to use async] */
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        // check the response status
+        if (response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+ 
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+            email,
+            password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+    
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+  
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
