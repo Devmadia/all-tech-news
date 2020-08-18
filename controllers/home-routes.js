1ac59050481a8b99689e06a3e0d8e04ad316991b
Devmadia <agathaphan@icloud.com>
2020-08-17 21:45:17 -0500

    finalized homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,20 +1,47 @@
 // this file will contain all the use-facing routes, such as homepage and login page
 
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    // replaces res.send() or res.sendFile for responses
-    res.render('homepage', {
-        id: 1,
-        post_url: 'https://handlebarsjs.com/guide/',
-        title: 'Handlebars Docs',
-        created_at: new Date(),
-        vote_count: 10,
-        comments: [{}, {}],
-        user: {
-          username: 'test_user'
-        }
-      });
+    // query set up to return all posts and their nested properties
+    Post.findAll({
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+        .then(dbPostData => {
+            // pass a single post object into the homepage template
+            console.log(dbPostData[0]);
+            /* don't need to  need to serialize data before when you built API routes, because the res.json() method automatically does that */
+            // res.render('homepage', dbPostData[0].get({ plain: true }));
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            // res.render('homepage', dbPostData[0]);
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
 });
 
 module.exports = router;
