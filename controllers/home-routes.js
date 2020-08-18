42dae381454af612681329a5be41e9771b0d711c
Devmadia <agathaphan@icloud.com>
2020-08-17 19:50:20 -0500

    updated homepage information to permit browser to make a single request to generate website

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,17 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     // replaces res.send() or res.sendFile for responses
-    res.render('homepage'); 
+    res.render('homepage', {
+        id: 1,
+        post_url: 'https://handlebarsjs.com/guide/',
+        title: 'Handlebars Docs',
+        created_at: new Date(),
+        vote_count: 10,
+        comments: [{}, {}],
+        user: {
+          username: 'test_user'
+        }
+      });
 });
 
 module.exports = router;
