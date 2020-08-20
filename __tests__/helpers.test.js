0770b6b80e5859398853eaaeff1d027fa05c4e99
Devmadia <agathaphan@icloud.com>
2020-08-19 22:04:43 -0500

    successful testing of format_plural function with jest

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,8 +1,14 @@
-const {format_date} = require('../utils/helpers');
+const {format_date, format_plural} = require('../utils/helpers');
 
 // a test in helpers.test.js to ensure format_date() takes Date() objects and returns dates in the MM/DD/YYYY format
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
   
     expect(format_date(date)).toBe('3/20/2020');
+});
+
+// test to check that format_plural() correctly pluralizes words
+test('format_plural() returns a pluralized word', () => {
+    expect(format_plural('tiger', 2)).toBe('tigers');
+	expect(format_plural('lion', 1)).toBe('lion');
 });
